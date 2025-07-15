0a1d189bd32eefc413cf8e0f641e74fb7dd609cc
shresthasriv <shresthas04.ms@gmail.com>
2025-07-15 17:07:39 +0530

    added config

---
+++ config/config.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY', '')
+DEEPSEEK_BASE_URL = 'https://api.deepseek.com'
+DEEPSEEK_MODEL = 'deepseek-chat'
+
+OUTPUT_DIR = 'outputs'
+PERSONA_TEMPLATE = """
+USER PERSONA ANALYSIS
+
+=== BASIC INFORMATION ===
+Age Range: {age_range}
+Location: {location}
+Occupation/Field: {occupation}
+
+=== PERSONALITY TRAITS ===
+{personality_traits}
+
+=== INTERESTS & HOBBIES ===
+{interests}
+
+=== COMMUNICATION STYLE ===
+{communication_style}
+
+=== VALUES & BELIEFS ===
+{values}
+
+=== BEHAVIORAL PATTERNS ===
+{behavior_patterns}
+
+=== GOALS & MOTIVATIONS ===
+{goals}
+
+=== CITATIONS ===
+{citations}
+""" 
